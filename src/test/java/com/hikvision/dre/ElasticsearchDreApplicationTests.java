fe1c1df7966e971389d0821dd2aabfe27a73065d
wangdd <iris19920918@163.com>
2019-01-31 14:51:14 +0800

    test: 测试application/x-www-form-urlencoded请求

--- src/test/java/com/hikvision/dre/ElasticsearchDreApplicationTests.java
+++ src/test/java/com/hikvision/dre/ElasticsearchDreApplicationTests.java
@@ -10,10 +10,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -34,5 +42,21 @@ public class ElasticsearchDreApplicationTests {
         logger.info("返回结果：{}", JSONObject.toJSONString(responseEntity));
     }
 
+    @Test
+    public void postTest() {
+        String url = "http://10.14.69.239:9999/artemis/api/salzburg/v1/componentModelManager/componentModelManagerQuery/queryComponentListByCid";
+        HttpMethod httpMethod = HttpMethod.POST;
+        MultiValueMap<String, String> queryBody = new LinkedMultiValueMap<>();
+        queryBody.add("c_component_id_list", "[\"dac\",\"nms\"]");
+        ResponseEntity<String> responseEntity = restTemplate.exchange(url, httpMethod, this.getRequestBody(queryBody), String.class);
+        logger.info("返回结果：{}", responseEntity.getBody());
+    }
+
+    public static <T> HttpEntity<T> getRequestBody(T requestStr) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.add("access_token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJzY29wZSJdLCJleHAiOjE1NDg5NTgwNjYsImp0aSI6ImRiYTJlNzVhLTA5OTAtNDY1Yy04MWRiLWRiZjFmMWZjM2Y2OSIsImNsaWVudF9pZCI6IjIyNjI5NzYwIn0.kpRZxiolOOl-QUD-JDZkRCm-uvib6aAHzwWzwRRXAmA");
+        return new HttpEntity<>(requestStr, headers);
+    }
 }
 
